17b6656e9953241d3be7ee150b57e4d76df2ef49
camilin87 <camilin87@gmail.com>
2015-01-22 13:26:56 -0500

    extract FibonacciProgram with the tests passing

---
+++ FibProgram/FibonacciProgram.cs
@@ -0,0 +1,34 @@
+﻿namespace FibProgram
+{
+    public class FibonacciProgram
+    {
+        private readonly IUserInteractor _interactor;
+
+        public FibonacciProgram(IUserInteractor interactor)
+        {
+            _interactor = interactor;
+        }
+
+        public void Run()
+        {
+            _interactor.DisplayMessage("Enter a number");
+            var fibIdx = ReadFibonacciIndex();
+
+            var result = new FibCalc().Get(fibIdx);
+            _interactor.DisplayMessage(string.Format("The number is {0}", result));
+        }
+
+        private int ReadFibonacciIndex()
+        {
+            var numberStr = _interactor.Read();
+
+            if (string.IsNullOrEmpty(numberStr))
+            {
+                return 0;
+            }
+
+            return int.Parse(numberStr);
+        }
+
+    }
+}
